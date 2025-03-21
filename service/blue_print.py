cf64070ce138c75ab5738321d9c3359a59b79df8
Jeremy Bolin <jeremymbolin@gmail.com>
2025-03-21 10:54:02 -0500

    Adds Additional Terminal Support

--- service/blue_print.py
+++ service/blue_print.py
@@ -23,7 +23,7 @@ bp = Blueprint("TerminalGateway", url_prefix="/api/terminal-gateway")
 #   },
 #   ...
 # }
-PAX_CONNECTIONS: Dict[str, Dict[str, Any]] = {}
+TERMINAL_CONNECTIONS: Dict[str, Dict[str, Any]] = {}
 
 
 @dataclass
@@ -36,6 +36,7 @@ class ChargeRequestInput(BaseRequestInput):
     amount: str
     laneId: str
     terminal_payment_type: str
+    terminal_type: str
 
 
 @bp.websocket("/")
@@ -81,16 +82,16 @@ async def pax_terminal_ws(request: Request, ws: Websocket):
                 await ws.close()
                 return
 
-            if terminal_id in PAX_CONNECTIONS.keys():
+            if terminal_id in TERMINAL_CONNECTIONS.keys():
 
                 # Preserve any pending requests in a variable
-                existing_pending = PAX_CONNECTIONS[terminal_id].get(
+                existing_pending = TERMINAL_CONNECTIONS[terminal_id].get(
                     "pending_requests", {}
                 )
 
                 # Now replace the old entry with the new WebSocket,
                 # and keep the existing pending requests if desired
-                PAX_CONNECTIONS[terminal_id] = {
+                TERMINAL_CONNECTIONS[terminal_id] = {
                     "websocket": ws,
                     "pending_requests": existing_pending,
                 }
@@ -104,17 +105,17 @@ async def pax_terminal_ws(request: Request, ws: Websocket):
                 logger.info(
                     f"Registering terminal_id in global dictionary: {terminal_id}"
                 )
-                PAX_CONNECTIONS[terminal_id] = {"websocket": ws, "pending_requests": {}}
+                TERMINAL_CONNECTIONS[terminal_id] = {"websocket": ws, "pending_requests": {}}
 
             # If it's a response to one of our requests, fulfill the future
             if (
                 request_id
-                and request_id in PAX_CONNECTIONS[terminal_id]["pending_requests"]
+                and request_id in TERMINAL_CONNECTIONS[terminal_id]["pending_requests"]
             ):
                 logger.info(
                     f"Attempting to resolve: {request_id} request at terminal: {terminal_id}"
                 )
-                fut = PAX_CONNECTIONS[terminal_id]["pending_requests"].pop(request_id)
+                fut = TERMINAL_CONNECTIONS[terminal_id]["pending_requests"].pop(request_id)
                 logger.info(
                     f"setting future response: {request_id} with data: {data}"
                 )
@@ -126,7 +127,7 @@ async def pax_terminal_ws(request: Request, ws: Websocket):
                     f"Received msg from {terminal_id}: {data}"
                 )
     except Exception:
-        logger.exception("Exception in pax websocket operations")
+        logger.exception("Exception in websocket operations")
 
 
 @bp.post("/charge")
@@ -148,7 +149,7 @@ async def charge(request: Request) -> JSONResponse:
         raise SanicException("Not authorized to perform this action.", status_code=401)
 
     logger.info(
-        f"Here are connected terminals: {PAX_CONNECTIONS.keys()}"
+        f"Here are connected terminals: {TERMINAL_CONNECTIONS.keys()}"
     )
     terminal_id = request_input.laneId
 
@@ -156,7 +157,7 @@ async def charge(request: Request) -> JSONResponse:
     max_retries = 3
 
     for attempt in range(1, max_retries + 1):
-        if terminal_id in PAX_CONNECTIONS:
+        if terminal_id in TERMINAL_CONNECTIONS:
             # Found it! Break out of the loop and proceed
             logger.info(
                 f"{terminal_id} found on attempt {attempt}"
@@ -169,7 +170,7 @@ async def charge(request: Request) -> JSONResponse:
 
                 logger.error(
                     f"Attempt {attempt}/{max_retries}: "
-                    f"Terminal {terminal_id} not found. Current connected terminals: {list(PAX_CONNECTIONS.keys())}"
+                    f"Terminal {terminal_id} not found. Current connected terminals: {list(TERMINAL_CONNECTIONS.keys())}"
                 )
                 logger.error(
                     f"Will retry {terminal_id} in {backoff_seconds} seconds..."
@@ -194,6 +195,7 @@ async def charge(request: Request) -> JSONResponse:
         "laneId": terminal_id,
         "amount": float(request_input.amount),
         "terminal_payment_type": request_input.terminal_payment_type,
+        "terminal_type": request_input.terminal_type,
     }
 
     def __evaluate_details(details: Dict[str, Any]) -> bool:
@@ -213,6 +215,23 @@ async def charge(request: Request) -> JSONResponse:
 
         return True
 
+    def __evaluate_new_pos_details(details: Dict[str, Any]) -> bool:
+        """
+        Basically tells us if a request is successful based on its details
+        :param details: Dict[str, Any]
+        :return: bool
+        """
+        device_response_code = details.get("AUTH_RESP")
+        if not device_response_code:
+            logger.error(f"No device response code present in: {details}")
+            raise ValueError("No Device Response Code Available In Response")
+
+        if device_response_code != "00":
+            logger.error(f"Non Zero Response Code In: {details}")
+            return False
+
+        return True
+
     # Create a Future so we can wait for the response
     fut = asyncio.get_event_loop().create_future()
 
@@ -220,10 +239,10 @@ async def charge(request: Request) -> JSONResponse:
     for attempt in range(1, 4):  # up to 3 attempts
         try:
             # Store the Future in pending_requests
-            PAX_CONNECTIONS[terminal_id]["pending_requests"][request_input.reference] = fut
+            TERMINAL_CONNECTIONS[terminal_id]["pending_requests"][request_input.reference] = fut
 
             # Pull the latest WebSocket in case it changed
-            ws = PAX_CONNECTIONS[terminal_id]["websocket"]
+            ws = TERMINAL_CONNECTIONS[terminal_id]["websocket"]
 
             # Attempt to send
             await ws.send(OG_JSON.dumps(message))
@@ -248,7 +267,7 @@ async def charge(request: Request) -> JSONResponse:
         logger.error(f"Unable to submit to lane id: {terminal_id}")
         # We can remove this future from pending_requests since we never sent the request
         # (No device can possibly respond to a message that never went out)
-        PAX_CONNECTIONS[terminal_id]["pending_requests"].pop(request_input.reference, None)
+        TERMINAL_CONNECTIONS[terminal_id]["pending_requests"].pop(request_input.reference, None)
 
         return json({
             "status": False,
@@ -279,7 +298,13 @@ async def charge(request: Request) -> JSONResponse:
         }
         """
         response_details = response.get("details")
-        is_successful = __evaluate_details(response_details)
+        if request_input.terminal_type == "PAX":
+            is_successful = __evaluate_details(response_details)
+        elif request_input.terminal_type == "NEWPOS":
+            is_successful = __evaluate_new_pos_details(response_details)
+        else:
+            is_successful = False
+
         return json({
             "status": is_successful,
             "message": response.get("message", ""),
@@ -293,7 +318,7 @@ async def charge(request: Request) -> JSONResponse:
     except asyncio.TimeoutError:
         return json({
             "status": False,
-            "message": "Timeout waiting for PAX terminal response",
+            "message": "Timeout waiting for terminal response",
             "laneId": terminal_id,
             "amount": request_input.amount,
         })
